4be242ab44b22b36213a5b497e255a8d6ff42faa
grassblock <hi@imgb.space>
2024-08-06 08:44:04 +0800

    fix: add ext dict

--- sync.sh
+++ sync.sh
@@ -3,6 +3,7 @@ RIME_HOME=~/.local/share/fcitx5/rime
 
 echo "正在复制 rime 配置至此..."
 cp $RIME_HOME/*.custom.yaml .
+cp $RIME_HOME/rime_ice_ext.dict.yaml .
 
 echo "正在复制字典..."
 cp $RIME_HOME/gdict/*.dict.yaml ./gdict
