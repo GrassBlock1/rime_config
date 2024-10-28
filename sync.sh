01a7f09fda1366f7092d2bc4438fd89a70c189e9
grassblock <hi@imgb.space>
2024-10-28 21:48:18 +0800

    chore: 更新脚本

--- sync.sh
+++ sync.sh
@@ -3,7 +3,9 @@ RIME_HOME=~/.local/share/fcitx5/rime
 
 echo "正在复制 rime 配置至此..."
 cp $RIME_HOME/*.custom.yaml .
+echo "正在复制扩展基础字典..."
 cp $RIME_HOME/rime_ice_ext.dict.yaml .
+cp $RIME_HOME/melt_eng_ext.dict.yaml .
 
 echo "正在复制字典..."
 cp $RIME_HOME/gdict/*.dict.yaml ./gdict
